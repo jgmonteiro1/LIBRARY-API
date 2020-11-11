4797d9dbd3e53c0080278db9a5bd030bb1ad6c7c
João Monteiro <joao.monteiro@aws.bpp>
2020-11-11 11:24:10 -0300

    Primeiro teste criado com devidas assertivas para um post

--- src/test/java/com/jgmonteiro/LibraryAPI/api/resource/BookControllerTest.java
+++ src/test/java/com/jgmonteiro/LibraryAPI/api/resource/BookControllerTest.java
@@ -1,14 +1,20 @@
 package com.jgmonteiro.LibraryAPI.api.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultHandlersDsl;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -19,10 +25,28 @@ public class BookControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    private static final String BOOK_API = "/api/books";
+
+
+
     @Test
     @DisplayName("Deve criar um livro com sucesso")
-    public void createBookTest(){
+    public void createBookTest() throws Exception{
 
+        String json = new ObjectMapper().writeValueAsString(null);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("json");
+
+        mvc.perform(request)
+        .andExpect(MockMvcResultMatchers.status().isCreated())
+        .andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("title").value("Livro1"))
+        .andExpect(MockMvcResultMatchers.jsonPath("author").value("Autor"))
+        .andExpect(MockMvcResultMatchers.jsonPath("isbn").value("123456789"));
     }
 
     @Test
